27762372247e363e3e868dc7543c903a6b330a07
Vicente Rivera <vicenterivera225@gmail.com>
2024-01-07 21:16:37 -0300

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
-
+import netlify from '@astrojs/netlify/functions';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
+  adapter: netlify()
 });
